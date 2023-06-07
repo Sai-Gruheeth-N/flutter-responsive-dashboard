2be6fe453669e7740838708846a1b06726f9a5e8
Gruheeth <gruheeth.288@gmail.com>
2023-06-07 15:00:48 +0530

    Create appbar and drawer for mobile scaffold

--- lib/responsive/mobile_scaffold.dart
+++ lib/responsive/mobile_scaffold.dart
@@ -10,8 +10,37 @@ class MobileScaffold extends StatefulWidget {
 class _MobileScaffoldState extends State<MobileScaffold> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.green,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade900,
+      ),
+      backgroundColor: Colors.grey.shade300,
+      drawer: Drawer(
+        backgroundColor: Colors.grey.shade300,
+        child: const Column(
+          children: [
+            DrawerHeader(
+              child: Icon(Icons.favorite),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('D A S H B O A R D'),
+            ),
+            ListTile(
+              leading: Icon(Icons.chat),
+              title: Text('M E S S A G E'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('S E T T I N G S'),
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('L O G O U T'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
