57e0410c08b8e5b961ef3b247be9b5b30ea05acb
Gruheeth <gruheeth.288@gmail.com>
2023-06-07 15:23:25 +0530

    Set responsive UI from tablet to desktop scaffold

--- lib/responsive/desktop_scaffold.dart
+++ lib/responsive/desktop_scaffold.dart
@@ -1,4 +1,6 @@
 import 'package:dashboard/constants.dart';
+import 'package:dashboard/util/my_box.dart';
+import 'package:dashboard/util/my_tile.dart';
 import 'package:flutter/material.dart';
 
 class DesktopScaffold extends StatefulWidget {
@@ -16,7 +18,56 @@ class _DesktopScaffoldState extends State<DesktopScaffold> {
       backgroundColor: myDefaultBackground,
       body: Row(
         children: [
+          // Open drawer
           myDrawer,
+
+          // Rest of the body
+          Expanded(
+            flex: 2,
+            child: Column(
+              children: [
+                // 4 boxes on the top
+                AspectRatio(
+                  aspectRatio: 4 / 1,
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: GridView.builder(
+                      itemCount: 4,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4,
+                      ),
+                      itemBuilder: (context, index) {
+                        return const MyBox();
+                      },
+                    ),
+                  ),
+                ),
+
+                // Tiles below it
+                Expanded(
+                  flex: 2,
+                  child: ListView.builder(
+                    itemCount: 5,
+                    itemBuilder: (context, index) {
+                      return const MyTile();
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.pink,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
